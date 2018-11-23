f4774de1f8fcecf5e51aa98235a1ae9fd5e16618
GIOVANNI DE LIMA CONCEICAO <100942064@ucsal.br>
2018-11-23 07:21:43 -0300

    criação da api

---
+++ qrsala_api/src/main/java/br/ucsal/edu/qrsala/QrsalaApplication.java
@@ -0,0 +1,12 @@
+package br.ucsal.edu.qrsala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrsalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrsalaApplication.class, args);
+	}
+}
