f1b0ca11a0d6b7e7ec8f292e9b1dc46d5b21c1d4
JeeanLiima97 <44836218+JeeanLiima97@users.noreply.github.com>
2018-12-07 03:19:28 +0000

    Alterações no sql

--- qrsala_api/src/main/resources/data.sql
+++ qrsala_api/src/main/resources/data.sql
@@ -1,14 +1,11 @@
-create table if not exists sala (ID int primary key, NOME varchar(30), NOME_PROFESSOR varchar(30), HORARIO_INICIO varchar(10), HORARIO_FIM varchar(10), DIASEMANA int);
-insert into sala (ID, NOME, NOME_PROFESSOR, HORARIO_INICIO, HORARIO_FIM, DIASEMANA) values(1,'204B', 'Mario Jorge', '7:00', '9:30', 1);
-insert into sala (ID, NOME, NOME_PROFESSOR, HORARIO_INICIO, HORARIO_FIM, DIASEMANA) values(2,'204B', 'Claudio Neiva', '9:50', '12:30', 1);
 insert into sala (ID, NOME, NOME_PROFESSOR, HORARIO_INICIO, HORARIO_FIM, DIASEMANA) values(3,'203B', 'Arnaldo Bispo', '07:0', '09:40', 2);
-insert into sala (ID, NOME, NOME_PROFESSOR, HORARIO_INICIO, HORARIO_FIM, DIASEMANA) values(4,'203B', 'Arnaldo Bispo', '09:40', '12:30', 2);
+insert into sala (ID, NOME, NOME_PROFESSOR, HORARIO_INICIO, HORARIO_FIM, DIASEMANA) values(4,'203B', 'Arnaldo Bispo', '09:50', '12:30', 2);
 
 
 insert into sala (ID, NOME, NOME_PROFESSOR, HORARIO_INICIO, HORARIO_FIM, DIASEMANA) values(5,'204B', 'Fernando', '07:0', '09:40', 2);
 insert into sala (ID, NOME, NOME_PROFESSOR, HORARIO_INICIO, HORARIO_FIM, DIASEMANA) values(6,'204B', 'Pedro', '10:0', '11:30', 2);
 insert into sala (ID, NOME, NOME_PROFESSOR, HORARIO_INICIO, HORARIO_FIM, DIASEMANA) values(7,'204B', 'João', '07:00', '12:30',4);
-insert into sala (ID, NOME, NOME_PROFESSOR, HORARIO_INICIO, HORARIO_FIM, DIASEMANA) values(8,'204B', 'Carlos', '07:00', '12:30', 4);
+insert into sala (ID, NOME, NOME_PROFESSOR, HORARIO_INICIO, HORARIO_FIM, DIASEMANA) values(8,'204B', 'Carlos', '07:00', '12:30', 5);
 
 insert into sala (ID, NOME, NOME_PROFESSOR, HORARIO_INICIO, HORARIO_FIM, DIASEMANA) values(9,'205B', 'Marcos Camera', '07:0', '09:40', 3);
 insert into sala (ID, NOME, NOME_PROFESSOR, HORARIO_INICIO, HORARIO_FIM, DIASEMANA) values(10,'205B', 'Carlos ', '09:50', '12:30', 3);
@@ -28,21 +25,20 @@ insert into sala (ID, NOME, NOME_PROFESSOR, HORARIO_INICIO, HORARIO_FIM, DIASEMA
 insert into sala (ID, NOME, NOME_PROFESSOR, HORARIO_INICIO, HORARIO_FIM, DIASEMANA) values(21,'208B', 'Neiva', '07:00', '09:40', 4);
 insert into sala (ID, NOME, NOME_PROFESSOR, HORARIO_INICIO, HORARIO_FIM, DIASEMANA) values(22,'208B', 'Carlos', '09:40', '12:30', 4);
 insert into sala (ID, NOME, NOME_PROFESSOR, HORARIO_INICIO, HORARIO_FIM, DIASEMANA) values(23,'208B', 'João', '07:00', '09:40', 1);
-insert into sala (ID, NOME, NOME_PROFESSOR, HORARIO_INICIO, HORARIO_FIM, DIASEMANA) values(24,'208B', 'Mateus', '09:40', '12:30', 1);
+insert into sala (ID, NOME, NOME_PROFESSOR, HORARIO_INICIO, HORARIO_FIM, DIASEMANA) values(24,'208B', 'Mateus', '09:50', '12:30', 1);
 
-insert into sala (ID, NOME, NOME_PROFESSOR, HORARIO_INICIO, HORARIO_FIM, DIASEMANA) values(25,'208B', 'Jose', '07:00', '09:40', 4);
+insert into sala (ID, NOME, NOME_PROFESSOR, HORARIO_INICIO, HORARIO_FIM, DIASEMANA) values(25,'208B', 'Jose', '07:00', '09:40', 4)
 insert into sala (ID, NOME, NOME_PROFESSOR, HORARIO_INICIO, HORARIO_FIM, DIASEMANA) values(26,'208B', 'Fernando', '09:50', '12:30', 4);
-insert into sala (ID, NOME, NOME_PROFESSOR, HORARIO_INICIO, HORARIO_FIM, DIASEMANA) values(27,'208B', 'Guilherme', '07:00', '09:40', 3);
+insert into sala (ID, NOME, NOME_PROFESSOR, HORARIO_INICIO, HORARIO_FIM, DIASEMANA) values(27,'208B', 'Guilherme', '07:00', '09:40', 3)
 insert into sala (ID, NOME, NOME_PROFESSOR, HORARIO_INICIO, HORARIO_FIM, DIASEMANA) values(28,'208B', 'Jorge', '09:50', '12:30', 3);
 
 
 insert into sala (ID, NOME, NOME_PROFESSOR, HORARIO_INICIO, HORARIO_FIM, DIASEMANA) values(29,'209B', 'Mario Jorge', '07:00', '09:40', 5);
 insert into sala (ID, NOME, NOME_PROFESSOR, HORARIO_INICIO, HORARIO_FIM, DIASEMANA) values(30,'209B', 'João', '09:50', '12:30', 5);
 insert into sala (ID, NOME, NOME_PROFESSOR, HORARIO_INICIO, HORARIO_FIM, DIASEMANA) values(31,'209B', 'Luiz', '07:00', '09:40', 1);
-insert into sala (ID, NOME, NOME_PROFESSOR, HORARIO_INICIO, HORARIO_FIM, DIASEMANA) values(32,'209B', 'Mario', '09:50', '12:30', 1);
+insert into sala (ID, NOME, NOME_PROFESSOR, HORARIO_INICIO, HORARIO_FIM, DIASEMANA) values(32,'209B', 'Mario', '09:50', '12:30', 1)
 
 insert into sala (ID, NOME, NOME_PROFESSOR, HORARIO_INICIO, HORARIO_FIM, DIASEMANA) values(33,'209B', 'Neiva', '07:05', '09:40', 5);
 insert into sala (ID, NOME, NOME_PROFESSOR, HORARIO_INICIO, HORARIO_FIM, DIASEMANA) values(34,'209B', 'Mario Jorge', '09:40', '12:30', 5);
 insert into sala (ID, NOME, NOME_PROFESSOR, HORARIO_INICIO, HORARIO_FIM, DIASEMANA) values(35,'209B', 'Rodrigo', '07:00', '09:40', 3);
-insert into sala (ID, NOME, NOME_PROFESSOR, HORARIO_INICIO, HORARIO_FIM, DIASEMANA) values(36,'209B', 'Jean', '09:40', '12:30', 3);
-
+insert into sala (ID, NOME, NOME_PROFESSOR, HORARIO_INICIO, HORARIO_FIM, DIASEMANA) values(36,'209B', 'Jean', '09:50', '12:30', 3);
